c09c864c4b127be14d83cd07c7eb103e1d7ff694
Santanu Talukdar <77837983+Santanu1507@users.noreply.github.com>
2023-08-01 17:05:30 +0530

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,9 +1,20 @@
+# Import the pyttsx3 library for text-to-speech functionality
 import pyttsx3
+# Import the sys module to access command-line arguments
 import sys
 
+# Get the text to be spoken from the command-line arguments
+# The text should be passed as an argument when running the script
 text = sys.argv[1]
 
+# Initialize the pyttsx3 engine for text-to-speech
 engine = pyttsx3.init()
+
+# Set the speech rate (speed) to 150 words per minute
 engine.setProperty('rate', 150)
+
+# Use the engine to say the provided text
 engine.say(text)
+
+# Wait for the speech to finish before exiting the script
 engine.runAndWait()
